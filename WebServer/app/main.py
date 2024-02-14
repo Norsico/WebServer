fa18b9777d937d0a4d93928ea45135ecddce7129
Norcoo <2542313773@qq.com>
2024-02-14 18:35:07 +0800

    plus

--- WebServer/app/main.py
+++ WebServer/app/main.py
@@ -15,14 +15,13 @@ def wxlogin():
         mydb = connect_to_db('key')
         result = select_data(mydb, 'wxkeys')
         for row in result:
-            if row[0] == 'root':
-                appid = row[1]
-                secret = row[2]
-                if row[3] == key:
+            if row["ID"] == 'root':
+                appid = row["appid"]
+                secret = row["secret"]
+                if row["connectKey"] == key:
                     url = (
                         f"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={js_code}"
                         f"&grant_type=authorization_code")
-
                     mydb.close()
                     response = requests.get(url)
                     return jsonify(response.json())
