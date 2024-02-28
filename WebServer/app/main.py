76d66d2dfb96959f438198169be31eaf697525a8
Norcoo <2542313773@qq.com>
2024-02-28 09:18:59 +0800

    add some explanations

--- WebServer/app/main.py
+++ WebServer/app/main.py
@@ -6,15 +6,27 @@ from flask import Flask, request, jsonify, send_from_directory, url_for
 
 from MySQL_Tools import *
 
+# 运用的是flask框架
 app = Flask(__name__)
 
 
+# 这里是记录git在服务器那边更新代码的指令，可以忽略，我只是记录下怕忘记
 # git fetch --all
 # git reset --hard origin/main
 
+
+# 增加接口就是这样的结构，例如下面这个就相当于我可以访问: http://172.17.156.158:6006/api/wxlogin
+# 这个172.17.156.158:6006前面的是服务器内网ip，后面的是端口号，外面访问将内网ip改成公网ip即可
 @app.route('/api/wxlogin', methods=['GET'])
-def wxlogin():
-    req = request.args.get('req')
+def wxlogin():  # 函数名任意
+    """
+    这个是微信小程序登录的接口，具体逻辑我也忘了，有点麻烦，先跳过这个
+    它这个登录的意思大概就是，我前端首先发送请求到微信的服务器，然后微信服务器返回一个code给我，
+    我再把这个code发送给我自己的服务器，然后我再根据这个code去微信服务器获取openid（这里要小程序的账户密钥之类的），
+    最后我再根据openid（微信用户唯一标识）去自己的数据库里查询是否有这个用户，没有的话就注册一个，
+    有的话就直接登录，然后把openid和用户名返回给我前端等等
+    """
+    req = request.args.get('req')  # 获取前端传来的参数，这里获取的是req参数，用request.args.get()这个方法，下面同理
     if req == 'GetOpenid':
         js_code = request.args.get('js_code')
         key = request.args.get('key')
@@ -51,12 +63,12 @@ def wxlogin():
         return jsonify({"code": 401, "msg": "fail"}), 401
 
 
-# 存储路径
+# 存储路径，这个文件夹在服务器里面有，用来储存用户上传的东西，但是我还没有做分类什么的，把上传的东西都放在一起了
 UPLOAD_FOLDER = '/userdata/upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-# 文件上传接口
+# 文件上传接口，这个是用户上传头像图片的接口，当时用AI写的，
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -76,20 +88,23 @@ def upload_file():
     return jsonify(response)
 
 
-# 获取图片接口
+# 获取图片接口，就相当于你输入一串比如http://0.0.0.0:1234/uploads/666.jpg这种的就会出现服务器对应的那张图片
+# 相当于一个图床
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
+# 这个是用户修改个人信息的接口，比如修改用户名啊，修改头像啊这些的
 @app.route('/changeUserdata', methods=['GET'])
 def changeUserdata():
-    changeWhat = request.args.get("changeWhat")
-    changeTo = request.args.get("changeTo")
-    openid = request.args.get("openid")
+    changeWhat = request.args.get("changeWhat")  # 改什么，数据是前端传过来的
+    changeTo = request.args.get("changeTo")  # 将数据改成什么
+    openid = request.args.get("openid")  # 唯一标识符，对应数据库中唯一的那个用户
     if openid != "":
-        if changeWhat == "avatar":
-            mydb = connect_to_db('UserData')
+        if changeWhat == "avatar":  # 改头像
+            mydb = connect_to_db('UserData')  # 这个connect_to_db()是我自己写的函数，其在本文件夹同级的MySQL_Tools.py文件中
+            # 下面也有几个是一样的
             update_data(mydb, "MainData", changeWhat, changeTo, "openid", openid)
             return jsonify({"message": "修改成功"})
         elif changeWhat == "UserName" and changeTo != "":
@@ -100,15 +115,17 @@ def changeUserdata():
             return jsonify({"message": "修改失败"})
 
 
+# 这个是我单独做的一个接口，用于删除用户制定好的计划
 @app.route('/changeUserdata/delPlan', methods=['GET'])
 def delPlan():
     openid = request.args.get("openid")
-    if openid !=  "":
+    if openid != "":
         name = request.args.get("name")
         mydb = connect_to_db('UserData')
+        # 这种的是数据库的操作，即使用Python操作数据库，用的是pymysql库，你可以去查看pymysql怎么使用的
         cursor = mydb.cursor()
-        cursor.execute(f"SELECT plans FROM MainData WHERE openid= '{openid}' ")
-        plans = json.loads(cursor.fetchall()[0]['plans'])
+        cursor.execute(f"SELECT plans FROM MainData WHERE openid= '{openid}' ")  # 像这个就是sql语句了，不会的可以问AI
+        plans = json.loads(cursor.fetchall()[0]['plans'])  # 像在实际开发中，一般会先对代码进行测试，在test文件夹内的文件一般就是用来测试的
         plans.pop(name)
         sql = f"UPDATE MainData SET plans = %s WHERE openid= '{openid}'"
         cursor.execute(sql, (json.dumps(plans),))
@@ -118,7 +135,7 @@ def delPlan():
         return jsonify({"message": "删除成功"})
 
 
-
+# 这个是用来添加用户制定好的计划的接口
 @app.route('/changeUserdata/addPlan', methods=['GET'])
 def addPlan():
     openid = request.args.get("openid")
@@ -132,7 +149,7 @@ def addPlan():
         cursor.execute(f"SELECT plans FROM MainData WHERE openid= '{openid}' ")
         plans = json.loads(cursor.fetchall()[0]['plans'])
         data = {
-            'way': eval(way),
+            'way': eval(way),  # 使用这个eval的原因是：服务器传过来的是一个str，但这里要转换成list
             'name': name,
             'note': note,
             'time': time,
@@ -140,7 +157,7 @@ def addPlan():
         if name in plans:
             cursor.close()
             mydb.close()
-            return jsonify({"message": "计划名重复"})
+            return jsonify({"message": "计划名重复"})  # 这里是为了没有计划名称重复的，要不然数据库可能会有些bug出现
         else:
             plans[name] = data
             sql = f"UPDATE MainData SET plans = %s WHERE openid= '{openid}'"
@@ -151,6 +168,7 @@ def addPlan():
             return jsonify({"message": "添加成功"})
 
 
+# 这个是用来获取用户数据的接口，里面可以拿到用户绝大部分的数据
 @app.route('/getUserdata', methods=['GET'])
 def getuserdata():
     openid = request.args.get("openid")
@@ -163,6 +181,7 @@ def getuserdata():
         return jsonify({"message": "获取失败"})
 
 
+# 这个是用户修改plan的接口，用于修改plan中的‘方式’，即教学方式
 @app.route('/fixPlan', methods=['GET'])
 def fixPlan():
     openid = request.args.get("openid")
@@ -185,4 +204,5 @@ def fixPlan():
 
 
 if __name__ == '__main__':
+    # 这里就是直接运行这个flask框架的应用了
     app.run(host='172.17.156.158', port=6006)
